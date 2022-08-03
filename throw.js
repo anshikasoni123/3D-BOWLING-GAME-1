04b4b45acce5d2ad6bc61aeb07c7f922ab108222
anshikasoni123 <80338855+anshikasoni123@users.noreply.github.com>
2022-08-03 19:46:31 +0530

    Add files via upload

---
+++ throw.js
@@ -0,0 +1,31 @@
+AFRAME.registerComponent("bowling-balls",{
+    init:function(){
+        this.bowlingBalls();
+    },
+    bowlingBalls:function(){
+        window.addEventListener("keydown",(e)=> {
+            if(e.key === "b"){
+                var ball = document.createElement('a-entity');
+                ball.setAttribute("geometry",{
+                    primitive:"sphere",
+                    radius:"0.1"
+                });
+                ball.setAttribute("material","color","black");
+                
+                var cam = document.querySelector("#camera");
+                pos = cam.getAttribute("position");
+
+                ball.setAttribute("position",{x:pos.x,y:pos.y,z:pos.z});
+
+                var camera = document.querySelector("#camera").object3D
+
+                var direction = new THREE.Vector3();
+                camera.getWorldDirection(direction);
+
+                ball.setAttribute("velocity",direction.multiplyScalar(-10));
+                var scene = document.querySelector("#scene");
+                scene.appendChild(ball);
+            }
+        })
+    }
+})
